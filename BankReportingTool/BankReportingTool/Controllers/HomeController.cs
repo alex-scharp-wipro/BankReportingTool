d237ab8561c2013e010a9252d779c94d00a80086
Alex Scharp <AL20074677@wipro.com>
2019-09-18 10:50:21 -0500

    Removed About and Contact pages

--- BankReportingTool/BankReportingTool/Controllers/HomeController.cs
+++ BankReportingTool/BankReportingTool/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace BankReportingTool.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         // GET /Home/Results
         public ActionResult Results()
         {
