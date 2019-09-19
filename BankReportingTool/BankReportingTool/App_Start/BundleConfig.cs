30d63911cc51e5df6d13f22a662da1655de04ae1
samuel-hamer <samuel.hamer@wipro.com>
2019-09-19 14:52:47 -0500

    Bootstrap 4 changes

--- BankReportingTool/BankReportingTool/App_Start/BundleConfig.cs
+++ BankReportingTool/BankReportingTool/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace BankReportingTool
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-materia.css",
                       "~/Content/site.css"));
         }
     }
