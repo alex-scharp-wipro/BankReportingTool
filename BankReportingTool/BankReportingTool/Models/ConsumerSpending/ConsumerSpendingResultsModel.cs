7e218b127ecf0589f64dfa9e64c421bcd5c63d39
Alex Scharp <AL20074677@wipro.com>
2019-09-23 10:35:18 -0500

    Added submit and results models.

--- BankReportingTool/BankReportingTool/Models/ConsumerSpending/ConsumerSpendingResultsModel.cs
+++ BankReportingTool/BankReportingTool/Models/ConsumerSpending/ConsumerSpendingResultsModel.cs
@@ -5,8 +5,19 @@ using System.Web;
 
 namespace BankReportingTool.Models.ConsumerSpending
 {
+    /// <summary>
+    /// Contains all necessary data and objects to display the generated 
+    /// report to the user.
+    /// This model is created by the report-generating service / API, and 
+    /// serves as the viewmodel of the Results view.
+    /// (Note: If this model becomes too complicated, we can create a separate
+    /// class to serve as the Results viewmodel.)
+    /// </summary>
     public class ConsumerSpendingResultsModel
     {
-        // TODO implement this
+        // TODO Add fields and properties
+
+        // TODO Add methods (if any)
+
     }
 }
--- BankReportingTool/BankReportingTool/Models/ConsumerSpending/ConsumerSpendingSubmitModel.cs
+++ BankReportingTool/BankReportingTool/Models/ConsumerSpending/ConsumerSpendingSubmitModel.cs
@@ -5,8 +5,16 @@ using System.Web;
 
 namespace BankReportingTool.Models.ConsumerSpending
 {
+    /// <summary>
+    /// Contains all necessary data and objects to generate the report.
+    /// This model is created in the Home controller's Submit action and is
+    /// sent to the report-generating service / API.
+    /// </summary>
     public class ConsumerSpendingSubmitModel
     {
-        // TODO implement this
+        // TODO Add fields and properties
+
+        // TODO Add methods (if any)
+
     }
 }
