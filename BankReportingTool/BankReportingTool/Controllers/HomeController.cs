c8031224161da639a32a9d18db0b64528d405390
Alex Scharp <AL20074677@wipro.com>
2019-09-23 10:22:52 -0500

    Added skeleton of Submit action to Home controller. Also added form and submit button in Filters partial view to test this action.

--- BankReportingTool/BankReportingTool/Controllers/HomeController.cs
+++ BankReportingTool/BankReportingTool/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BankReportingTool.Models;
+using BankReportingTool.Models.ConsumerSpending;
 
 namespace BankReportingTool.Controllers
 {
@@ -15,7 +16,7 @@ namespace BankReportingTool.Controllers
         }
 
         /// <summary>
-        /// GET /Home/GetFilters/reportId
+        /// GET /Home/GetFilters/{reportId}
         /// After the user selects a report, the page should request here.
         /// This method returns a partial view containing search filters 
         /// specific to the requested report.
@@ -34,7 +35,7 @@ namespace BankReportingTool.Controllers
                 // Consumer Spending Report
                 case (int)Reports.ConsumerSpending:
                     viewName = "ConsumerSpendingFilters";
-                    viewModel = new Models.ConsumerSpending.ConsumerSpendingFiltersModel();
+                    viewModel = new ConsumerSpendingFiltersModel();
                     break;
 
                 // Uh oh! Id does not match a known report.
@@ -46,17 +47,37 @@ namespace BankReportingTool.Controllers
             // Store reportId in ViewBag.
             ViewBag.SelectedReport = reportId;
 
-            // Return the appropriate partial view
+            // Return correct filters partial view
             return PartialView(viewName, viewModel);
         }
 
+        // POST /Home/Submit
+        [HttpPost]
+        public ActionResult Submit(object filtersModel)
+        {
+            // TODO: Type check model, and verify it matches selected report.
+            // TODO: Also check model and data validity.
+
+            // TODO: Construct submit model from filters model and data.
+            object submitModel = null;
+
+            // TODO: Send submit model to service/API to process report.
+
+            // TODO: Send report results to Results action.
+            object resultsModel = null;
+
+            return View("Results", resultsModel);
+        }
+
         // GET /Home/Results
-        public ActionResult Results()
+        // TODO: Delete this action once Submit is finished. Results page should only be accessible from Submit action.
+        public ActionResult Results(object resultsModel)
         {
             ViewBag.Message = "Results Query";
 
             return View();
         }
+
         // Grab chart to render in partial view
         public ActionResult DrawChart()
         {
